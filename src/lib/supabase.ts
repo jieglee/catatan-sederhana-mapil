a8b084c12ef44803e5b7e3518b10b6efb6c1c1bd
jieglee <callmezie23@gmail.com>
2026-07-27 13:32:37 +0700

    supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,14 +3,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 // Ganti dengan URL & anon key project Supabase lu sendiri
-const supabaseUrl = 'https://xxxxxxxxxxxx.supabase.co';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://mixrkazrdrhzpcqolmap.supabase.co';
+const supabaseAnonKey = 'sb_publishable_TUWzl-Nk4pdarkU5qba-Zg_DC46tUxi';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
 });
